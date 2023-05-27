6059d810ce29115e2ae22a77ea2d4d1e0b01a3d9
Ayumi <ayumi.xie5@gmail.com>
2023-05-27 14:31:55 +0700

    Penambahan menu

--- FlavaProject/src/App.java
+++ FlavaProject/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Menu: ");
+        System.out.println("1. Rekomendasi");
+        System.out.println("2. Search & Filter");
+        System.out.println("3. Keranjang Belanja");
+        System.out.println("4. My Order");
+        System.out.println("5. Promo dan Pembayaran");
+        System.out.println("6. Notifikasi");
     }
 }
