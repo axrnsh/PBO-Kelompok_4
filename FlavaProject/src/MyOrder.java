c17dab91c9a475052b6b207dd6afa75074618096
Angelafayola <angelafayola964@gmail.com>
2023-05-27 15:19:55 +0700

    Create MyOrder.java

---
+++ FlavaProject/src/MyOrder.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// untuk bagian myorder yang berisi history, pelacakan, dan nama pelanggan
+class MyOrder {
+    // OrderId untuk melacak paket yg dipesan, customername untuk mengidentifikasi nama customer, 
+    // dan items untuk histori pesanan
+
+    private String orderId;
+    private String customerName;
+    private List<String> items;
+
+    // this untuk mengakses object 
+    public MyOrder(String orderId, String customerName, List<String> items) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.items = items;
+    }
+
+    // mengisi data ke dalam object
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+}
+
+// fungsi untuk melacak pesanan 
+class OrderTracker {
+    private Map<String, MyOrder> orderMap;
+    private List<MyOrder> orderHistory;
+
+    public OrderTracker() {
+        orderMap = new HashMap<>();
+        orderHistory = new ArrayList<>();
+    }
+
+    public void placeOrder(MyOrder order) {
+        orderMap.put(order.getOrderId(), order);
+        orderHistory.add(order);
+        System.out.println("Order placed successfully. Order ID: " + order.getOrderId());
+    }
+
+    public void trackOrder(String orderId) {
+        if (orderMap.containsKey(orderId)) {
+            MyOrder order = orderMap.get(orderId);
+            System.out.println("Order ID: " + order.getOrderId());
+            System.out.println("Customer Name: " + order.getCustomerName());
+            System.out.println("Items: " + order.getItems());
+        } else {
+            System.out.println("Order not found.");
+        }
+    }
+}
+ 
