c4d6de75a7e5bec2a6276df1592e52c802e595ba
vanessa292929 <134504361+vanessa292929@users.noreply.github.com>
2023-06-12 14:46:43 +0700

    update penjual

--- FlavaProject/src/penjual.java
+++ FlavaProject/src/penjual.java
@@ -2,13 +2,15 @@ public class penjual {
 
     private String namaPenjual;
     private String DaftarProduk;
+    private double ratingPenjual;
 
     public penjual() {
     }
 
-    public penjual(String namaPenjual, String daftarProduk) {
+    public penjual(String namaPenjual, String DaftarProduk, double ratingPenjual) {
         this.namaPenjual = namaPenjual;
-        this.DaftarProduk = daftarProduk;
+        this.DaftarProduk = DaftarProduk;
+        this.ratingPenjual = ratingPenjual;
     }
 
     public String getNamaPenjual() {
@@ -27,12 +29,19 @@ public class penjual {
         this.DaftarProduk = DaftarProduk;
     }
 
-    @Override
-    public String toString() {
-        return "{" +
-                " namaPenjual='" + getNamaPenjual() + "'" +
-                ", DaftarProduk='" + getDaftarProduk() + "'" +
-                "}";
+    public double getRatingPenjual() {
+        return this.ratingPenjual;
     }
 
+    public void setRatingPenjual(double ratingPenjual) {
+        this.ratingPenjual = ratingPenjual;
+    }
+
+    // Override metode toString()
+    @Override
+    public String toString() {
+        return "Nama Penjual: " + namaPenjual + "\n"
+                + "Daftar Produk: " + DaftarProduk + "\n"
+                + "Rating Penjual: " + ratingPenjual;
+    }
 }
