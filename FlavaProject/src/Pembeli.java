ef5f5867dfc1680383c7b8044d4d9ade4ca8d6b4
vanessa292929 <134504361+vanessa292929@users.noreply.github.com>
2023-06-13 12:52:29 +0700

    update pembeli

--- FlavaProject/src/Pembeli.java
+++ FlavaProject/src/Pembeli.java
@@ -2,20 +2,23 @@
  * class ini berfungsi untuk menyimpan identitas pembeli
  */
 
-
 public class Pembeli {
 
     private String namaPembeli;
     private String DaftarProduk;
     private String AlamatPembeli;
+    private double ratingProduk;
+    private String ulasan;
 
     public Pembeli() {
     }
 
-    public Pembeli(String namaPembeli, String daftarProduk, String AlamatPembeli) {
+    public Pembeli(String namaPembeli, String daftarProduk, String AlamatPembeli, double ratingProduk, String ulasan) {
         this.namaPembeli = namaPembeli;
         this.DaftarProduk = daftarProduk;
         this.AlamatPembeli = AlamatPembeli;
+        this.ratingProduk = ratingProduk;
+        this.ulasan = ulasan;
     }
 
     public String getNamaPembeli() {
@@ -42,11 +45,34 @@ public class Pembeli {
         this.AlamatPembeli = AlamatPembeli;
     }
 
+    public String getAlamatPembeli() {
+        return this.AlamatPembeli;
+    }
+
+    public double getRatingProduk() {
+        return this.ratingProduk;
+    }
+
+    public void setRatingProduk(double ratingProduk) {
+        this.ratingProduk = ratingProduk;
+    }
+
+    public String getUlasan() {
+        return this.ulasan;
+    }
+
+    public void setUlasan(String ulasan) {
+        this.ulasan = ulasan;
+    }
+
     @Override
     public String toString() {
         return "{" +
                 " namaPembeli='" + getNamaPembeli() + "'" +
                 ", DaftarProduk='" + getDaftarProduk() + "'" +
+                ", AlamatPembeli='" + getAlamatPembeli() + "'" +
+                ", ratingProduk='" + getRatingProduk() + "'" +
+                ", ulasan='" + getUlasan() + "'" +
                 "}";
     }
 
