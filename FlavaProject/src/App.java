bc612a842c2823a26ab24b1d44f9d95842357b8b
vanessa292929 <134504361+vanessa292929@users.noreply.github.com>
2023-06-13 12:58:02 +0700

    update App

--- FlavaProject/src/App.java
+++ FlavaProject/src/App.java
@@ -17,20 +17,20 @@ public class App {
         Kategori kat2 = new Kategori("Rok", "Olympia Comfease Skirt", "DUST");
         Kategori kat3 = new Kategori("Sepatu", "Millie Heels", "Billini");
 
-        Produk blouse = new Produk("White Blouse",  500000, "Baju masih baru");
-        Produk celanabiru = new Produk("Blue Jeans",  400000, "Celana bekas");
-        Produk sepatu = new Produk("Yellow Sun",  600000, "Sepstu masih baru");
+        Produk blouse = new Produk("White Blouse", 500000, "Baju masih baru");
+        Produk celanabiru = new Produk("Blue Jeans", 400000, "Celana bekas");
+        Produk sepatu = new Produk("Yellow Sun", 600000, "Sepstu masih baru");
 
-        Pembeli pembeli1 = new Pembeli("ayumi", "baju,celana,topi", "jln uph no 21");
-        Pembeli pembeli2 = new Pembeli("destha", "celana 2pcs", "jln uph no 12");
-        Pembeli pembeli3 = new Pembeli("vanessa", "baju, topi", "jln uph no 190");
+        Pembeli pembeli1 = new Pembeli("ayumi", "baju,celana,topi", "jl. uph no 21", 4.5, "bagus");
+        Pembeli pembeli2 = new Pembeli("destha", "celana 2pcss", "jl. uph no 12", 5, "baik");
+        Pembeli pembeli3 = new Pembeli("vanessa", "baju, topi", "jl. uph no 190", 8, "good");
 
-        Penjual penjual1 = new Penjual("chris", "topi", 7);
-        Penjual penjual2 = new Penjual("lisa", "baju", 5);
-        Penjual penjual3 = new Penjual("Michael", "celana", 6);
-        
-        Transaksi transaksi1 = new Transaksi("TRX001", "Rene","2023-06-12", 500000);
-        Transaksi transaksi2 = new Transaksi("TRX002", "Moona","2023-06-15", 750000);
-        Transaksi transaksi3 = new Transaksi("TRX003", "Risu","2023-06-20", 1000000);
+        Penjual penjual1 = new Penjual("chris", "topi celine", 7);
+        Penjual penjual2 = new Penjual("lisa", "croptop", 5);
+        Penjual penjual3 = new Penjual("Michael", "celana jeans", 6);
+
+        Transaksi transaksi1 = new Transaksi("TRX001", "Rene", "2023-06-12", 500000);
+        Transaksi transaksi2 = new Transaksi("TRX002", "Moona", "2023-06-15", 750000);
+        Transaksi transaksi3 = new Transaksi("TRX003", "Risu", "2023-06-20", 1000000);
     }
 }
